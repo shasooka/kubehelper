8c3d2be83ef9c351b6776a7d37217ae5a2a29bcd
Shantanu <k.shantanu2006@gmail.com>
2020-01-20 17:18:47 +0000

    Get pods

--- src/main/java/com/shantanu/kubehelper/KubeController.java
+++ src/main/java/com/shantanu/kubehelper/KubeController.java
@@ -1,14 +1,27 @@
 package com.shantanu.kubehelper;
 
-// import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// import org.springframework.stereotype.Controller;
+import io.kubernetes.client.models.V1Pod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class KubeController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Kube Helper";
+	private final ListPods service;
+	public KubeController(ListPods service){
+		this.service = service;
+	}
+
+
+	@RequestMapping("/listpods")
+	public @ResponseBody
+	List<String> greeting() throws Exception
+	{
+		return service.getpods();
 	}
 }
