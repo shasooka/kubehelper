5800e09b830786e4e0c06bb685a0cdc71449decb
Shantanu <k.shantanu2006@gmail.com>
2020-01-22 17:04:45 +0000

    Removed unused class and methods.

--- src/main/java/com/shantanu/kubehelper/CreateNamespace.java
+++ src/main/java/com/shantanu/kubehelper/CreateNamespace.java
@@ -4,13 +4,11 @@ import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
 import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
-//import io.kubernetes.client.models.V1Namespace;
-//import io.kubernetes.client.models.V1NamespaceBuilder;
-//import io.kubernetes.client.models.V1NamespaceList;
 import io.kubernetes.client.models.V1Namespace;
 import io.kubernetes.client.models.V1ObjectMeta;
-import io.kubernetes.client.util.ClientBuilder;
-import io.kubernetes.client.util.KubeConfig;
+//import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.Config;
+//import io.kubernetes.client.util.KubeConfig;
 import org.springframework.stereotype.Service;
 
 import java.io.FileReader;
@@ -21,13 +19,7 @@ import java.io.IOException;
 public class CreateNamespace {
     public String createnamespace(String id) throws IOException, ApiException {
 
-        String kubeConfigPath = "/Users/kumshant0/.kube/config";
-
-        // loading the out-of-cluster config, a kubeconfig from file-system
-        ApiClient client =
-                ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
-
-        // set the global default api-client to the in-cluster one from above
+        ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
 
         try {
--- src/main/java/com/shantanu/kubehelper/ListNamespaces.java
+++ src/main/java/com/shantanu/kubehelper/ListNamespaces.java
@@ -1,5 +1,6 @@
 package com.shantanu.kubehelper;
 
+import io.kubernetes.client.util.Config;
 import org.springframework.stereotype.Service;
 import io.kubernetes.client.ApiClient;
 import io.kubernetes.client.ApiException;
@@ -7,40 +8,22 @@ import io.kubernetes.client.Configuration;
 import io.kubernetes.client.apis.CoreV1Api;
 //import io.kubernetes.client.models.V1Namespace;
 import io.kubernetes.client.models.V1NamespaceList;
-import io.kubernetes.client.util.ClientBuilder;
-import io.kubernetes.client.util.KubeConfig;
+//import io.kubernetes.client.util.ClientBuilder;
+//import io.kubernetes.client.util.KubeConfig;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 @Service
 public class ListNamespaces {
     public V1NamespaceList getservices() throws IOException, ApiException {
 
-        String kubeConfigPath = "/Users/kumshant0/.kube/config";
-
-        // loading the out-of-cluster config, a kubeconfig from file-system
-        ApiClient client =
-                ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath))).build();
-
-        // set the global default api-client to the in-cluster one from above
+        ApiClient client = Config.defaultClient();
         Configuration.setDefaultApiClient(client);
-
         CoreV1Api api = new CoreV1Api();
         V1NamespaceList list = api.listNamespace(null, null, null, null, null, null, null, null, null);
         return list;
-
-
-//        V1NamespaceList list =
-//                api.listNamespace(null, null, null, null, null, null, null, null, null);
-//        List<String> pods = new ArrayList<>();
-//        for (V1NamespaceList item : list.getItems()) {
-//            System.out.println(item.getMetadata().getContinue());
-//
-//            pods.add(item.getMetadata().getName());
-//        }
-//        return pods;
     }
 }
